b18a1f15fcdff2f0c6724ddcb92fa75d01096a06
remeeh <remeeh@gmail.com>
2017-03-24 01:15:49 +0100

    adding false query test

--- Repetition/src/test/scala-2.12/datastructures/ProbeTableSpec.scala
+++ Repetition/src/test/scala-2.12/datastructures/ProbeTableSpec.scala
@@ -12,13 +12,27 @@ class ProbeTableSpec extends FlatSpec with Matchers {
   "Query " should "return results given a valid query" in {
     val rnd = new Random(System.currentTimeMillis())
     val hp = new Hyperplane(6, rnd.nextLong(), 128)
-    val vec = Array.fill[Float](128)(rnd.nextFloat)
-    val id = 1
-    val tuple = (1, vec)
+    val vec = (1,Array.fill[Float](128)(rnd.nextFloat))
+    val vec1 = (2,Array.fill[Float](128)(rnd.nextFloat))
+    val vec2 = (3,Array.fill[Float](128)(rnd.nextFloat))
 
     val pt = new ProbeTable(hp)
-    pt+=tuple
+    pt+=vec
+    pt+=vec1
+    pt+=vec2
 
-    assert(pt.query(vec).head == tuple) // same object
+    assert(pt.query(vec._2).contains(vec)) // same object
+    assert(pt.query(vec1._2).contains(vec1)) // same object
+    assert(pt.query(vec2._2).contains(vec2)) // same object
   }
+  "Query " should "not throw exception given invalid query" in {
+    val rnd = new Random(System.currentTimeMillis())
+    val hp = new Hyperplane(6, rnd.nextLong(), 128)
+    val vec = (1,Array.fill[Float](128)(rnd.nextFloat))
+    val vec2 = (3,Array.fill[Float](128)(rnd.nextFloat))
+    val pt = new ProbeTable(hp)
+    pt+=vec
+    assert(!pt.query(vec2._2).contains(vec2)) // same object
+  }
+
 }
